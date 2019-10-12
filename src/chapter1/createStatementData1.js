49a670477496433d30585030979b1b794a78d7e3
zhaoFangyi <fangyieagle@qq.com>
2019-10-12 17:42:03 +0800

    第一章

--- src/chapter1/createStatementData1.js
+++ src/chapter1/createStatementData1.js
@@ -9,57 +9,17 @@ export default function createStatementData (invoice, plays) {
   return result
 }
 
-// 创建演出计算器 performanceCalculator
-class PerformanceCalculator {
-  constructor(aPerformance, aPlay) {
-    this.performance = aPerformance
-    this.play = aPlay
-  }
-  get amount () {
-    let result = 0
-    switch (this.play.type) {
-      case 'tragedy':
-        result = 4000
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 3000
-        if (this.performance.audience > 20) {
-          result += 1000 + 500 * (this.performance.audience - 20)
-        }
-        result += 300 * this.performance.audience
-        break
-      default:
-        throw new Error(`unknow type: ${this.type}`)
-    }
-    return result
-  }
-}
-
 function enrichPerformance(aPerformance) {
-  const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+  const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
   const result = Object.assign({}, aPerformance)
   result.play = calculator.play
-  result.amount = amountFor(result)
-  result.volumeCredits = volumeCreditsFor(result)
+  result.amount = calculator.amount
+  result.volumeCredits = calculator.volumeCredits
   return result
 }
 
 function playFor(aPerformance) {
   return plays[aPerformance.playID]
-}
-
-function amountFor(aPerformance) {
-  return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0
-  result += Math.max(aPerformance.audience -30, 0)
-  if (aPerformance.play.type === 'comedy') result += Math.floor(aPerformance.audience / 5)
-  return result
 }
 
 function totalAmount (data) {
@@ -70,6 +30,55 @@ function totalVolumeCredits (data) {
   return data.performances
     .reduce((total, p) => total + p.volumeCredits, 0)
 }
+
+function createPerformanceCalculator (aPerformance, aPlay) {
+  switch (aPlay.play) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay)  
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay)  
+    default:
+      throw new Error(`unknow type: ${aPlay.type}`)
+  }
+}
+
+// 创建演出计算器 performanceCalculator
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance
+    this.play = aPlay
+  }
+  get amount () {
+    throw new Error('subclass responsibility')
+  }
+  get volumeCredits() {
+    return Math.max(this.performance.audience -30, 0)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount () {
+    let result = 4000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
+    }
+    return result
+  }
+}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount () {
+    let result = 3000
+    if (this.performance.audience > 20) {
+      result += 1000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+}
+
 
 
 
